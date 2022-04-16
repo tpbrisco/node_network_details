5d26ecc1daef08191f30cdeca682e362f5732bb5
tpbrisco <tpbrisco+gh@gmail.com>
2022-04-16 11:16:08 -0400

    better logging, detection of where we cant run

--- node_network_details.sh
+++ node_network_details.sh
@@ -8,29 +8,36 @@ set -euo pipefail
 # default values
 INTERFACES="*"
 
-# check utilities - e.g. versions of "ip" have different capabilities
-set +e
-ip --json addr show >/dev/null 2>/dev/null
-set -e
-if [ $? -eq 0 ]; then
-    BINIP="ip"
-else
-    echo "\"ip\" does not support the --json flag"
-    exit 1
-fi
-JQ=$(type -p jq)
-if [ "$JQ" == '' ]; then
-    echo "\"jq\" is needed"
-    exit 1
-fi
 DEBUG=1
-
 # debug logging
 function dbg_log () {
     if [ "$DEBUG" -eq 1 ]; then
-	echo $@ >> /tmp/node_network_details.log
+	echo $@ >> /tmp/node_network_details-dbg.log
     fi
 }
+
+# error logging
+function err_log () {
+    echo $@ >> /tmp/node_network_details-err.log
+}
+
+# check utilities - e.g. versions of "ip" have different capabilities
+set +e
+ip --json addr show >/dev/null 2>&1
+ip_ok=$?
+set -e
+if [ $ip_ok -eq 0 ]; then
+    BINIP="ip"
+    IPJSON=1
+else
+    err_log "\"ip\" does not support the --json flag"
+    IPJSON=0
+fi
+JQ=$(type -p jq)
+if [ "$JQ" == '' ]; then
+    err_log "\"jq\" is needed"
+    exit 1
+fi
 
 DATE="$(date)"
 dbg_log "$DATE"
@@ -41,7 +48,11 @@ function show_interface_data () {
     dbg_log "Showing data"
     echo "# HELP node_network_details A list of interfaces and IP addresses"
     echo "# TYPE node_network_details gauge"
-    $BINIP --json addr show | jq -Mr '.[] |.ifname as $ifname |.ifindex as $ifindex|.address as $macaddr |.addr_info[] | "node_network_details{ifname=\"\($ifname)\", macaddr=\"\($macaddr)\", addressfamily=\"\(.family)\", address=\"\(.local)\"} \($ifindex)"'
+    if [[ "$IPJSON" == 1 ]]; then
+	$BINIP --json addr show | jq -Mr '.[] |.ifname as $ifname |.ifindex as $ifindex|.address as $macaddr |.addr_info[] | "node_network_details{ifname=\"\($ifname)\", macaddr=\"\($macaddr)\", addressfamily=\"\(.family)\", address=\"\(.local)\"} \($ifindex)"'
+    else
+	true
+    fi
 }
 
 show_interface_data > /tmp/node_net-$$.txt
